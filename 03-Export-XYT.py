419e51b5a11655854014d0516797a54aa783e62a
Oceanbreak <oceanbreak@gmail.com>
2024-08-17 10:30:36 +0000

    Add export XYT

---
+++ 03-Export-XYT.py
@@ -0,0 +1,24 @@
+# from lib.PictureViewer import PictureViewer
+
+# img = PictureViewer((300, 500), 'Test canvas')
+# img.show()
+
+from lib.SonarData import SonarData
+import glob
+
+xtf_files = glob.glob('test/*.xtf')
+
+for xtf_file in xtf_files:
+
+    out_name = xtf_file[:-4] + '.csv'
+    sonar_data = SonarData(xtf_file)
+    trackout = sonar_data.getXYT()
+
+    with open(out_name, 'w') as fw:
+        for line in trackout:
+            fw.write(';'.join(line.split(';')[1:]) + '\n')
+    print('Done')
+
+    cable_out = sonar_data.extractCableOut()
+    # print(f'Cable out: {cable_out}')
+
