a4757dd00acb67efa3e9ee623a698a58f4b50c42
Oceanbreak <oceanbreak@gmail.com>
2024-08-17 10:19:41 +0000

    Generating XY csv files

---
+++ 01-Export-WGS84.py
@@ -0,0 +1,34 @@
+from lib.Settings import Settings
+from lib.SonarData import SonarData
+import os
+from tkinter.filedialog import askdirectory
+import glob
+
+
+
+if __name__ == '__main__':
+
+    # Initiate settings
+    settings = Settings()
+    print(settings)
+    path = askdirectory(initialdir=settings.directory)
+    settings.directory = path
+    settings.writefile()
+    print(settings)
+
+    # Load XTF files
+    xtf_list = glob.glob(os.path.join(settings.directory, '*.xtf')) + \
+                glob.glob(os.path.join(settings.directory, '*.XTF'))
+    print(xtf_list)
+
+    # Process data
+    for xtf_file in xtf_list:
+        track_file = '.'.join(xtf_file.split('.')[:-1]) + '.csv'
+        sonar_data = SonarData(xtf_file)
+        track = sonar_data.extractTrackWGS84()
+        with open(track_file, 'w') as wfile:
+            for line in track:
+                wfile.write(f'{line[0]};{line[1]}\n')
+        print(f'Writing file {track_file} done')
+
+    
