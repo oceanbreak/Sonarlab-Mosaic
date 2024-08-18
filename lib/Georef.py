641264708ffcfe2ecf83574ad3aa70aa01959ea0
Oceanbreak <oceanbreak@gmail.com>
2024-08-18 10:14:06 +0000

    Add surfer gsr2 file making

--- lib/Georef.py
+++ lib/Georef.py
@@ -11,16 +11,40 @@ class Georef:
                 'Version=1\n' \
                 'Method=3\n' \
                 'Name,Raster X,Raster Y,Longitude,Latitude,Active\n'
+    
+    WGS_gsr_header = f'Golden Software Cartographic Reference Information : Version 2.00\n' + \
+                        'System Type : GCS\n' + \
+                        'System Identification : World Geodetic System 1984\n' + \
+                        '  Comment : System Identification strings are subject to change at any time.\n' + \
+                        'System GCS Token : GEOGCS_DATUM_WGS84\n' + \
+                        'System POSC/EPSG Code : 4326\n' + \
+                        'Projection Method : Unprojected Lat/Long\n' + \
+                        'Datum Name : World Geodetic System 1984\n' + \
+                        'Datum GCS Token : DATUM_WGS84\n' + \
+                        'Datum Method : None\n' + \
+                        'Datum Ellipsoid Type : WGS 84\n' + \
+                        'Datum Ellipsoid GCS Token : ELLIPSOID_WGS_84\n' + \
+                        'Datum Ellipsoid Radius (meters) : 6378137\n' + \
+                        'Datum Ellipsoid Flattening : 0.00335281066474748\n'
 
-    def __init__(self, out_file : str, image_coord_margins : np.ndarray):
-        self.image_coord_margins = image_coord_margins
-        self.out_file_name = out_file
 
-    def make(self):
-        with open(self.out_file_name, 'w') as fw:
+    def __init__(self,):
+        pass
+
+
+    def make(self,  out_file : str, image_coord_margins : np.ndarray):
+        with open(out_file, 'w') as fw:
             fw.write(self.header)
-            for line in self.image_coord_margins:
+            for line in image_coord_margins:
                 fw.write(f',{int(line[0])},{int(line[1])},{line[2]},{line[3]},1\n')
         print('Reference file created')
 
 
+    def makeSurferGeorefWGS84(self, out_file):
+        with open(out_file, 'w') as fw:
+            fw.write(self.WGS_gsr_header)
+
+
+if __name__ == '__main__':
+    g = Georef('out', (2,3))
+    print(g.WGS_gsr_header)
