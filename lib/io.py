02ec9027c5a34bba135c158668e4076f65db03d3
Oceanbreak <oceanbreak@gmail.com>
2024-08-18 11:51:07 +0000

    Add FileNaming class

--- lib/io.py
+++ lib/io.py
@@ -1,4 +1,30 @@
 import numpy as np
+import os
+
+class FileNaming:
+
+    def __init__(self, xtf_file):
+        self.xtf_file_name = xtf_file
+
+    def get_xtf_name(self):
+        return self.xtf_file_name
+    
+    def get_track_WGS_name(self):
+        return '.'.join(self.xtf_file_name.split('.')[:-1]) + '.csv'
+    
+    def get_track_GK_name(self):
+        return '.'.join(self.xtf_file_name.split('.')[:-1]) + '-GK.csv'
+    
+    def get_track_georef_name(self):
+        return self.get_track_WGS_name() + '.gsr2'
+    
+    def get_map_name(self):
+        return '.'.join(self.xtf_file_name.split('.')[:-1]) + '_mosaic.png'
+    
+    def get_map_georef_name(self):
+        return self.get_map_name() + '.georef'
+    
+
 
 
 
@@ -24,7 +50,7 @@ def loadCsvGK(in_name : str, delimeter=';') -> np.ndarray:
     with open(in_name, 'r') as f_read:
         for line in f_read:
             line = line.strip()
-            _, _,in_x, in_y = line.split(delimeter)
+            in_x, in_y = line.split(delimeter)
             x.append(float(in_x))
             y.append(float(in_y))
 
