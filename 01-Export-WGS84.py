9d39c9f542ca3dad52329218a9dde2d63512ea6d
Oceanbreak <oceanbreak@gmail.com>
2024-08-18 10:14:50 +0000

    Add gsr2 making

--- 01-Export-WGS84.py
+++ 01-Export-WGS84.py
@@ -1,5 +1,6 @@
 from lib.Settings import Settings
 from lib.SonarData import SonarData
+from lib.Georef import Georef
 import os
 from tkinter.filedialog import askdirectory
 import glob
@@ -24,11 +25,14 @@ if __name__ == '__main__':
     # Process data
     for xtf_file in xtf_list:
         track_file = '.'.join(xtf_file.split('.')[:-1]) + '.csv'
+        georef = track_file + '.gsr2'
         sonar_data = SonarData(xtf_file)
         track = sonar_data.extractTrackWGS84()
         with open(track_file, 'w') as wfile:
             for line in track:
                 wfile.write(f'{line[0]};{line[1]}\n')
         print(f'Writing file {track_file} done')
+        gr = Georef()
+        gr.makeSurferGeorefWGS84(georef)
 
     
