39b3d72a3d1e20d565a92fd88f45493c61a9cf36
Oceanbreak <oceanbreak@gmail.com>
2024-08-22 17:21:37 +0000

    Add askdirectory

--- 03-Export-XYT.py
+++ 03-Export-XYT.py
@@ -5,8 +5,11 @@
 
 from lib.SonarData import SonarData
 import glob
+from tkinter.filedialog import askdirectory
+import os
 
-xtf_files = glob.glob('test/*.xtf')
+path = askdirectory()
+xtf_files = glob.glob(os.path.join(path, '*.xtf'))
 
 for xtf_file in xtf_files:
 
@@ -14,11 +17,14 @@ for xtf_file in xtf_files:
     sonar_data = SonarData(xtf_file)
     trackout = sonar_data.getXYT()
 
+
+        
+
     with open(out_name, 'w') as fw:
         for line in trackout:
             fw.write(';'.join(line.split(';')[1:]) + '\n')
     print('Done')
 
     cable_out = sonar_data.extractCableOut()
-    # print(f'Cable out: {cable_out}')
+    print(f'Cable out: {cable_out}')
 
