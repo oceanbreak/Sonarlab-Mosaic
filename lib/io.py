52d5d3c3e149a75afb8b8b5802d83def816094bd
Ivan Anisimov <oceanbreak@gmail.com>
2025-03-12 21:32:15 +0300

    Creates files in subfolders

--- lib/io.py
+++ lib/io.py
@@ -3,32 +3,53 @@ import os
 
 class FileNaming:
 
+    proc_name = 'proc'
+    out_name = 'mosaic'
+
     def __init__(self, xtf_file):
         self.xtf_file_name = xtf_file
+        path, fname = os.path.split(xtf_file)
+        self.base_path = path
+        self.base_name = fname
+
+        # add folders
+        self.proc_folder = os.path.join(self.base_path, 'proc')
+        self.output_folder = os.path.join(self.base_path, 'mosaic')
+
+        self.createFolders()
 
     def get_xtf_name(self):
         return self.xtf_file_name
     
     def get_track_WGS_name(self):
-        return '.'.join(self.xtf_file_name.split('.')[:-1]) + '.csv'
+        outputfile =  '.'.join(self.base_name.split('.')[:-1]) + '.csv'
+        return os.path.join(self.proc_folder, outputfile)
     
     def get_track_GK_name(self):
-        return '.'.join(self.xtf_file_name.split('.')[:-1]) + '-GK.csv'
+        outputfile =  '.'.join(self.base_name.split('.')[:-1]) + '-GK.csv'
+        return os.path.join(self.proc_folder, outputfile)
     
     def get_track_georef_name(self):
         return self.get_track_WGS_name() + '.gsr2'
     
     def get_map_name(self):
-        return '.'.join(self.xtf_file_name.split('.')[:-1]) + '_mosaic.png'
+        outputfile =  '.'.join(self.base_name.split('.')[:-1]) + '_mosaic.png'
+        return os.path.join(self.output_folder, outputfile)
     
     def get_geotiff_name(self):
-        return '.'.join(self.xtf_file_name.split('.')[:-1]) + '_mosaic.tif'
+        outputfile =  '.'.join(self.base_name.split('.')[:-1]) + '_mosaic.tif'
+        return os.path.join(self.output_folder, outputfile)
     
     def get_map_georef_name(self):
         return self.get_map_name() + '.georef'
     
 
-
+    def createFolders(self):
+        if not os.path.exists(self.proc_folder):
+            os.mkdir(self.proc_folder)
+        if not os.path.exists(self.output_folder):
+            os.mkdir(self.output_folder)
+    
 
 
 def npToCsv(out_name : str, array : np.ndarray):
